24b748fd08d2b8528d554abd76f6960b4fd00a13
patrickezera <pzanatta37@gmail.com>
2024-05-06 15:25:27 -0300

    Altera server

--- src/server.js
+++ src/server.js
@@ -19,6 +19,5 @@ server.use(
 server.use(router); //rotas na prota 3000
 
 server.listen(8000, () => {
-  console.log("Boas-vindas a API do Allbooks");
-  console.log("API disponível através da URL http://localhost:8000");
+  console.log("API disponível em http://localhost:8000");
 });
